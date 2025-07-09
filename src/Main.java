6569a711fc6306eb65c8244b44eba01bd6a5cdbe
TAT <chernob.alex@yandex.ru>
2025-07-09 18:17:50 +0300

    commit new price

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,10 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int ticketPrice =20275; // стоимость билета в рублях
+        int mileCost = 20;       // рублей за 1 милю
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int miles = ticketPrice / mileCost; // целочисленное деление, дробные мили отбрасываются
+
+        System.out.println("Начислено миль: " + miles);
     }
 }
